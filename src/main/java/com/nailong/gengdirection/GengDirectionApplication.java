4f3366a3c34abe15e0e38f08e30002b2a3b7c095
CavanasD <m18404288789@163.com>
2026-04-19 08:56:21 +0800

    [Initial | 20260419_01] Initial Commit

---
+++ src/main/java/com/nailong/gengdirection/GengDirectionApplication.java
@@ -0,0 +1,13 @@
+package com.nailong.gengdirection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GengDirectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GengDirectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nailong/gengdirection/GengDirectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nailong.gengdirection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GengDirectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
